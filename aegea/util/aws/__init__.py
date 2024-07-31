0acd8567f2efdecff432f7914224f01db325b726
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-07-30 22:43:45 -0700

    Fix import error

--- aegea/util/aws/__init__.py
+++ aegea/util/aws/__init__.py
@@ -99,6 +99,8 @@ def ensure_vpc():
 
 
 def availability_zones():
+    from ... import config
+
     for az in clients.ec2.describe_availability_zones()["AvailabilityZones"]:
         if az["ZoneId"] in config.vpc.avoid_az_ids:
             continue
